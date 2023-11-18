d302797f8da7a96f4b1cb7ba22bdb1d7cbc71dc5
@MaD Max <shyam13112@gmail.com>
2023-11-18 08:17:46 +0530

    Exercise Question

---
+++ Practice-DSA/Exercise/Fibonacci.java
@@ -0,0 +1,13 @@
+// To calculate Fibonacci Series up to n numbers.
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+    }
+    
+}
