da5ae60c13535ff11db2760d4f2aeb20a2e24f8f
@MaD Max <shyam13112@gmail.com>
2023-11-24 10:08:42 +0530

    Exercise Question

---
+++ Practice-DSA/Exercise/Grettingmsg.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Grettingmsg {
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter your name here: ");
+    String name = sc.next();
+    System.out.println("Hello "+name+" Nice to meet u!");
+    }
+}
