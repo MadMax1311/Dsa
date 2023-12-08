0f5516b9232d0d1828353882a63bf04f9fc2e381
@MaD Max <shyam13112@gmail.com>
2023-12-08 13:43:04 +0530

    DSA Practice

---
+++ Practice-DSA/Function/Function.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Function {
+    public static void main(String[] args) {
+        gretting();
+    }
+    static void gretting(){
+        System.out.println("Hello World");
+    }
+}
