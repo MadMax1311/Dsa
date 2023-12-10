dec6f9dea3439bceaef3cb82c43b9200d7e8dab8
@MaD Max <shyam13112@gmail.com>
2023-12-10 14:34:13 +0530

    DSA Practice

---
+++ Practice-DSA/Function/function3.java
@@ -0,0 +1,9 @@
+public class function3 {
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+    static String greet(){
+        String gt = "How are you";
+        return gt;
+    }
+}
