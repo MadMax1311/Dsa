60f11a3ed1265d33db3b0d6cac2f587cf657b76c
@MaD Max <shyam13112@gmail.com>
2023-12-23 23:31:15 +0530

    Basic_java

---
+++ Practice-DSA/General_Problems/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        System.out.println(n);
+
+        Float fl = sc.nextFloat();
+        System.out.println(fl);
+    }
+}
