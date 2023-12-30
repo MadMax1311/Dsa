ca72efa3ad42c9ef0ac16876bc6933791fa020a8
@MaD Max <shyam13112@gmail.com>
2023-12-30 23:10:57 +0530

    Basic_java

---
+++ Practice-DSA/Basic_progrmas/Arrays.java
@@ -0,0 +1,11 @@
+package Basic_progrmas;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int [] arr ;// here int is datatype , arr is refrence variable ,, its work in compile time
+        
+        arr = new int [6]; //creating the object in heap memory ,, its work in run time
+        // here new keyword is used to create an object in heap memory
+        
+    }
+}
