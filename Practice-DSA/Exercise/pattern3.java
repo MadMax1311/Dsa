6f9ba4caa9cba43293fb573a713c9c91acb28efd
@MaD Max <shyam13112@gmail.com>
2024-03-20 17:42:53 +0530

    first commit

---
+++ Practice-DSA/Exercise/pattern3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pattern3 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+    // int n=4;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    
+}
+}
